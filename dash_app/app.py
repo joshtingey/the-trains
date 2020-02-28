f8fa4eb7d78f3b7af15921e9a47fee6a8704e0ab
Josh Tingey <joshtingey93@gmail.com>
2020-02-28 11:47:39 +0000

    feat: added database connection to dash application

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from flask_sqlalchemy import SQLAlchemy
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
+
+server.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(server)
 
 app.layout = html.Div(children=[
     html.H1(children='thetrains.co.uk'),
@@ -21,8 +27,10 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': [1, 2, 3], 'y': [4, 1, 2],
+                    'type': 'bar', 'name': 'Train1'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5],
+                    'type': 'bar', 'name': 'Train2'},
             ],
             'layout': {
                 'title': 'thetrains.co.uk example visualisation'
