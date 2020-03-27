5c3899aa1f63e1561441c2aa4ba3941e29faa247
Josh Tingey <joshtingey93@gmail.com>
2020-03-27 12:24:10 +0000

    feat: certificates are now used and added simple database test paths

--- dash/app.py
+++ dash/app.py
@@ -12,15 +12,44 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-#server.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-#server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#db = SQLAlchemy(server)
+server.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(server)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String())
+    surname = db.Column(db.String())
+
+
+@server.route('/db_test')
+def db_test():
+    status = 'old'
+    db.create_all()
+    user = User.query.first()
+    if not user:
+        u = User(name='The', surname='Trains')
+        db.session.add(u)
+        db.session.commit()
+        status = 'new'
+    user = User.query.first()
+    return "User '{} {}' in database ({}). ".format(
+        user.name, user.surname, status)
+
+
+@server.route('/db_drop')
+def db_drop():
+    db.drop_all()
+    return "All tables dropped."
+
 
 app.layout = html.Div(children=[
-    html.H1(children='thetrains.co.uk'),
+    html.H1(children='thetrains'),
 
     html.Div(children='''
-        thetrains.co.uk dash application
+        thetrains dash application
     '''),
 
     dcc.Graph(
@@ -33,7 +62,7 @@ app.layout = html.Div(children=[
                     'type': 'bar', 'name': 'Train2'},
             ],
             'layout': {
-                'title': 'thetrains.co.uk example visualisation'
+                'title': 'thetrains example visualisation'
             }
         }
     )
