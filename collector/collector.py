93be557655d9d81a86df294c3d404f7c0922b141
Josh Tingey <joshtingey93@gmail.com>
2020-03-30 09:12:02 +0100

    feat: added postgreSQL database intergration to the collector, ppm records are now saved into a new table

--- collector/collector.py
+++ collector/collector.py
@@ -7,13 +7,43 @@ from time import sleep
 from datetime import datetime
 import json
 
+from sqlalchemy import create_engine  
+from sqlalchemy import Column, Integer, Float  
+from sqlalchemy.ext.declarative import declarative_base  
+from sqlalchemy.orm import sessionmaker
 import stomp  
+
+base = declarative_base()
+
+
+class PPM(base):
+    __tablename__ = 'ppm'
+
+    date = Column(Integer, primary_key=True)
+    total = Column(Integer)
+    on_time = Column(Integer)
+    late = Column(Integer)
+    ppm = Column(Float)
+    rolling_ppm = Column(Float)
 
 
 class Collector(object):
-    def __init__(self, conn):
-        self.conn = conn
+    def __init__(self, session):
+        self.session = session
         self.counter = 0
+        self.init()
+
+    def init(self):
+        # TODO: Use the heartbeat protocol
+        self.conn = stomp.Connection(
+            host_and_ports=[('datafeeds.networkrail.co.uk', 61618)],
+            keepalive=True,
+            vhost='datafeeds.networkrail.co.uk'
+        )
+
+        self.conn.set_listener('', self)
+        signal.signal(signal.SIGINT, self.exit_handler)
+        signal.signal(signal.SIGTERM, self.exit_handler)
 
     def connect_and_subscribe(self):
         # We use an exponential backoff and a max number of retry limit
@@ -45,25 +75,34 @@ class Collector(object):
             id=headers['message-id'],
             subscription=headers['subscription']
         )
-        print('Message: {}, Subscription: {}'.format(
-            headers['message-id'], headers['subscription']))
 
         # Parse the message JSON and extract the required fields
         parsed = json.loads(message)
-        timestamp = parsed['RTPPMDataMsgV1']['timestamp']
+        timestamp = int(parsed['RTPPMDataMsgV1']['timestamp']) / 1000
         parsed = parsed['RTPPMDataMsgV1']['RTPPMData']['NationalPage']
-        total = parsed['NationalPPM']['Total']
-        on_time = parsed['NationalPPM']['OnTime']
-        late = parsed['NationalPPM']['Late']
-        ppm = parsed['NationalPPM']['PPM']['text']
-        rolling_ppm = parsed['NationalPPM']['RollingPPM']['text']
+        total = int(parsed['NationalPPM']['Total'])
+        on_time = int(parsed['NationalPPM']['OnTime'])
+        late = int(parsed['NationalPPM']['Late'])
+        ppm = float(parsed['NationalPPM']['PPM']['text'])
+        rolling_ppm = float(parsed['NationalPPM']['RollingPPM']['text'])
 
-        date = datetime.fromtimestamp(int(timestamp)/1000.0)
+        date = datetime.fromtimestamp(timestamp)
         date = date.strftime('%Y-%m-%d %H:%M:%S')
 
         print('{}: ({},{},{}), ({},{})'.format(
             date, total, on_time, late, ppm, rolling_ppm
         ))
+
+        ppm_record = PPM(
+            date=timestamp,
+            total=total,
+            on_time=on_time,
+            late=late,
+            ppm=ppm,
+            rolling_ppm=rolling_ppm)
+
+        self.session.add(ppm_record)
+        self.session.commit()
 
     def on_error(self, headers, message):
         print('received an error "{}"'.format(message))
@@ -76,21 +115,23 @@ class Collector(object):
     def exit_handler(self, sig, frame):
         print('Disconnecting...')
         self.conn.disconnect()
+        self.session.close()
         sys.exit(0)
 
 
 def main():
-    # TODO: Use the heartbeat protocol
-    conn = stomp.Connection(
-        host_and_ports=[('datafeeds.networkrail.co.uk', 61618)],
-        keepalive=True,
-        vhost='datafeeds.networkrail.co.uk'
-    )
+    try:
+        db = create_engine(os.environ["DATABASE_URL"])
+        print(db.table_names())
+    except Exception:
+        print("Can't connect to database!")
+        sys.exit(0)
 
-    collector = Collector(conn)
-    conn.set_listener('', collector)
-    signal.signal(signal.SIGINT, collector.exit_handler)
-    signal.signal(signal.SIGTERM, collector.exit_handler)
+    Session = sessionmaker(db)
+    session = Session()
+    base.metadata.create_all(db)
+
+    collector = Collector(session)
     collector.connect_and_subscribe()
 
     while 1:
