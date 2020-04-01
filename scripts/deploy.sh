ccf076afdb96513a629c371306392d3a2a05d9d7
Josh Tingey <joshtingey93@gmail.com>
2020-04-01 09:47:13 +0100

    fix: deployment script now caused a rollout of the new images

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,8 +15,8 @@ sed -i "s/<DB_PASS>/${DB_PASS}/g" ./manifests/dash/config.yaml
 sed -i "s/<DB_NAME>/${DB_NAME}/g" ./manifests/dash/config.yaml
 
 kubectl apply -f manifests/database
-kubectl apply -f manifests/collector
-kubectl apply -f manifests/dash
+kubectl rollout restart deployment collector -n thetrains
+kubectl rollout restart deployment dash -n thetrains
 
 sed -i "s/${DB_USER}/<DB_USER>/g" ./manifests/database/config.yaml
 sed -i "s/${DB_PASS}/<DB_PASS>/g" ./manifests/database/config.yaml
