05a42a38cb384eb3767d74ed7841c85bc855890e
Josh Tingey <joshtingey93@gmail.com>
2020-05-24 15:10:45 +0100

    feat: added basic TM feed implementation and tidied how message types are handled

--- collector/collector.py
+++ collector/collector.py
@@ -26,62 +26,50 @@ log = logging.getLogger("collector")
 
 
 class Feeds(enum.Enum):
-    """
-    Enumeration detailing the feed implementations.
+    """Enumeration detailing the feed implementations.
     """
     PPM = 0
     TD = 1
+    TM = 2
 
 
 class StompFeed(object):
-    """
-    Base feed handling class, which all others derive from.
+    """Base feed handling class, which all others derive from.
     """
 
-    def __init__(self, name, topic, durable, mongo):
-        """
-        Initialise the StompFeed.
+    def __init__(self, topic, durable, mongo):
+        """Initialise the StompFeed.
 
         Args:
-            name (int): 'Feeds' enumeration number
+            name (int): "Feeds" enumeration number
             topic (str): NR topic string
             durable (str): Durable connection name
             mongo (common.mongo.Mongo): Database class
         """
-        self.name = name
         self.topic = topic
         self.durable = durable
         self.mongo = mongo
 
     async def handle_message(self, message):
-        """
-        Handle the JSON message, overide in dervied classes
+        """Handle the JSON message, override in derived classes
         """
         raise NotImplementedError
 
 
 class PPMFeed(StompFeed):
-    """
-    PPM feed handling class.
+    """Public performance metric feed handling class.
     """
 
     def __init__(self, mongo):
-        """
-        Initialise the PPMFeed.
+        """Initialise the PPMFeed.
 
         Args:
             mongo (common.mongo.Mongo): Database class
         """
-        super().__init__(
-            Feeds.PPM,
-            "/topic/RTPPM_ALL",
-            "thetrains-ppm",
-            mongo
-        )
+        super().__init__("/topic/RTPPM_ALL", "thetrains-ppm", mongo)
 
     async def handle_message(self, message):
-        """
-        Handle the PPM JSON message.
+        """Handle the PPM JSON message.
         """
         try:
             parsed = orjson.loads(message)
@@ -89,19 +77,19 @@ class PPMFeed(StompFeed):
             log.error("Can't decode STOMP message")
             return
 
-        nat = parsed['RTPPMDataMsgV1']['RTPPMData']['NationalPage']
+        nat = parsed["RTPPMDataMsgV1"]["RTPPMData"]["NationalPage"]
         doc = {
             "date": datetime.datetime.fromtimestamp(
-                int(parsed['RTPPMDataMsgV1']['timestamp'])/1000),
-            "total": int(nat['NationalPPM']['Total']),
-            "on_time": int(nat['NationalPPM']['OnTime']),
-            "late": int(nat['NationalPPM']['Late']),
-            "ppm": float(nat['NationalPPM']['PPM']['text']),
-            "rolling_ppm": float(nat['NationalPPM']['RollingPPM']['text'])
+                int(parsed["RTPPMDataMsgV1"]["timestamp"])/1000),
+            "total": int(nat["NationalPPM"]["Total"]),
+            "on_time": int(nat["NationalPPM"]["OnTime"]),
+            "late": int(nat["NationalPPM"]["Late"]),
+            "ppm": float(nat["NationalPPM"]["PPM"]["text"]),
+            "rolling_ppm": float(nat["NationalPPM"]["RollingPPM"]["text"])
         }
 
-        log.debug('{}: ({},{},{}), ({},{})'.format(
-            doc["date"].strftime('%Y-%m-%d %H:%M:%S'),
+        log.debug("{}: ({},{},{}), ({},{})".format(
+            doc["date"].strftime("%Y-%m-%d %H:%M:%S"),
             doc["total"],
             doc["on_time"],
             doc["late"],
@@ -114,27 +102,19 @@ class PPMFeed(StompFeed):
 
 
 class TDFeed(StompFeed):
-    """
-    TD feed handling class.
+    """Train describer feed handling class.
     """
 
     def __init__(self, mongo):
-        """
-        Initialise the TDFeed.
+        """Initialise the TDFeed.
 
         Args:
             mongo (common.mongo.Mongo): Database class
         """
-        super().__init__(
-            Feeds.TD,
-            "/topic/TD_LNW_C_SIG_AREA",
-            "thetrains-c-td",
-            mongo
-        )
+        super().__init__("/topic/TD_LNW_C_SIG_AREA", "thetrains-td", mongo)
 
     async def handle_message(self, message):
-        """
-        Handle the TD JSON message.
+        """Handle the TD JSON message.
         """
         try:
             parsed = orjson.loads(message)
@@ -143,18 +123,21 @@ class TDFeed(StompFeed):
             return
 
         for msg in parsed:
-            if "CA_MSG" in msg.keys():
-                step = msg["CA_MSG"]
-                if step["area_id"] == "MP":
+            msg_type = msg.keys()
+            msg = msg[msg_type]
+            if msg_type == "CA_MSG":
+                # berth step
+                # TODO: Get a list of "Train Describers" from file
+                if msg["area_id"] == "MP":
                     doc = {
                         "date": datetime.datetime.fromtimestamp(
-                            int(step["time"])/1000),
-                        "from": str(step["area_id"] + step["from"]),
-                        "to": str(step["area_id"] + step["to"])
+                            int(msg["time"])/1000),
+                        "from": str(msg["area_id"] + msg["from"]),
+                        "to": str(msg["area_id"] + msg["to"])
                     }
 
                     log.debug("time: {}, from: {}, to: {}".format(
-                        doc["date"].strftime('%Y-%m-%d %H:%M:%S'),
+                        doc["date"].strftime("%Y-%m-%d %H:%M:%S"),
                         doc["from"],
                         doc["to"]
                     ))
@@ -162,13 +145,88 @@ class TDFeed(StompFeed):
                     if self.mongo is not None:
                         self.mongo.add("td", doc)
 
+            elif msg_type == "CB_MSG":
+                # berth cancel
+                pass
+            elif msg_type == "CC_MSG":
+                # berth interpose
+                pass
+            elif msg_type == "CT_MSG":
+                # heartbeat
+                pass
+            elif msg_type == "SF_MSG":
+                # signalling update
+                pass
+            elif msg_type == "SG_MSG":
+                # signalling refresh
+                pass
+            elif msg_type == "SH_MSG":
+                # signalling refresh finished
+                pass
+            else:
+                # should not happen
+                log.warning("Received unknown TD message type")
+
+
+class TMFeed(StompFeed):
+    """Train movement feed handling class.
+    """
+
+    def __init__(self, mongo):
+        """Initialise the TMFeed.
+
+        Args:
+            mongo (common.mongo.Mongo): Database class
+        """
+        super().__init__("/topic/TRAIN_MVT_ED_TOC", "thetrains-tm", mongo)
+
+    async def handle_message(self, message):
+        """Handle the TD JSON message.
+        """
+        try:
+            parsed = orjson.loads(message)
+        except orjson.JSONDecodeError:
+            log.error("Can't decode STOMP message")
+            return
+
+        for msg in parsed:
+            msg_type = msg["header"]["msg_type"]
+            msg = msg["body"]
+            if msg_type == "0001":
+                # train activation
+                pass
+            elif msg_type == "0002":
+                # train cancellation
+                pass
+            elif msg_type == "0003":
+                # train movement
+                pass
+            elif msg_type == "0004":
+                # unidentified train (not used in production)
+                pass
+            elif msg_type == "0005":
+                # train reinstatement
+                pass
+            elif msg_type == "0006":
+                # change of origin
+                pass
+            elif msg_type == "0007":
+                # change of identity
+                pass
+            elif msg_type == "0008":
+                # change of location
+                pass
+            else:
+                # should not happen
+                log.warning("Received unknown TM message type")
+
 
 def get_feed(feed, mongo):
     """
     Get the feed handling class.
 
     Args:
-        feed: 'Feeds' enumeration number
+        feed: "Feeds" enumeration number
         mongo (common.mongo.Mongo): Database class
     Returns:
         StompFeed: Feed handler class
@@ -177,8 +235,10 @@ def get_feed(feed, mongo):
         return PPMFeed(mongo)
     elif feed is Feeds.TD:
         return TDFeed(mongo)
+    elif feed is Feeds.TM:
+        return TMFeed(mongo)
     else:
-        log.warning("Don't recongnise feed name")
+        log.warning("Don't recognise feed name")
         return None
 
 
@@ -201,20 +261,15 @@ class STOMPCollector(object):
         self.attempts = config.CONN_ATTEMPTS  # Max number of conn attempts
         self.nr_user = config.NR_USER  # Network rail username
         self.nr_pass = config.NR_PASS  # Network rail password
-        self.init()
 
-    def init(self):
-        """
-        Initialise the stomp connection and signal handlers.
-        """
         try:  # Setup the STOMP connection to network rail feed
             self.conn = stomp.Connection(
-                host_and_ports=[('datafeeds.networkrail.co.uk', 61618)],
+                host_and_ports=[("datafeeds.networkrail.co.uk", 61618)],
                 keepalive=True,
-                vhost='datafeeds.networkrail.co.uk',
+                vhost="datafeeds.networkrail.co.uk",
                 heartbeats=(100000, 100000)
             )
-            self.conn.set_listener('handler', self)  # Register self
+            self.conn.set_listener("handler", self)  # Register self
         except Exception as e:
             log.warning("STOMP setup error ({}), continue anyway".format(e))
             self.conn = None
@@ -233,11 +288,11 @@ class STOMPCollector(object):
                 self.conn.subscribe(
                     self.feeds[feed].topic,
                     self.feeds[feed].durable,
-                    ack='client-individual',
-                    headers={'activemq.subscriptionName':
+                    ack="client-individual",
+                    headers={"activemq.subscriptionName":
                              self.feeds[feed].durable}
                 )
-                log.info("Subscribed to {}".format(self.feeds[feed].name))
+                log.info("Subscribed to {}".format(feed))
             except Exception as e:
                 log.error("STOMP subscription error ({})".format(e))
 
@@ -255,7 +310,7 @@ class STOMPCollector(object):
                     self.conn.connect(
                         username=self.nr_user,
                         passcode=self.nr_pass,
-                        wait=True, headers={'client-id': self.nr_user}
+                        wait=True, headers={"client-id": self.nr_user}
                     )
                 else:
                     log.warning("No STOMP connection to connect to")
@@ -275,7 +330,7 @@ class STOMPCollector(object):
         Subscribe to a Network Rail STOMP feed.
 
         Args:
-            name (int): 'Feeds' enumeration number
+            name (int): "Feeds" enumeration number
         """
         feed = get_feed(name, self.mongo)
 
@@ -288,8 +343,8 @@ class STOMPCollector(object):
                 self.conn.subscribe(
                     feed.topic,
                     feed.durable,
-                    ack='client-individual',
-                    headers={'activemq.subscriptionName': feed.durable}
+                    ack="client-individual",
+                    headers={"activemq.subscriptionName": feed.durable}
                 )
                 self.feeds[name] = feed
                 log.info("Subscribed to feed ({})".format(name))
@@ -315,9 +370,9 @@ class STOMPCollector(object):
         """
         STOMP on_message handler.
         """
-        log.debug('Got message')
-        self.conn.ack(id=headers['message-id'],
-                      subscription=headers['subscription'])
+        log.debug("Got message")
+        self.conn.ack(id=headers["message-id"],
+                      subscription=headers["subscription"])
 
         for name, feed in self.feeds.items():
             if str(feed.topic) == str(headers["destination"]):
@@ -340,21 +395,21 @@ class STOMPCollector(object):
         """
         STOMP on_error handler.
         """
-        log.error('STOMP connection error "{}"'.format(message))
+        log.error("STOMP connection error '{}'".format(message))
         self.connect_and_subscribe()
 
     def on_disconnected(self):
         """
         STOMP on_disconnected handler.
         """
-        log.error('STOMP connection disconnect')
+        log.error("STOMP connection disconnect")
         self.connect_and_subscribe()
 
     def on_heartbeat_timeout(self):
         """
         STOMP on_heartbeat_timeout handler.
         """
-        log.error('STOMP connection heartbeat timeout')
+        log.error("STOMP connection heartbeat timeout")
         self.connect_and_subscribe()
 
 
@@ -376,9 +431,12 @@ def main():
     if conf.TD_FEED:
         collector.subscribe(Feeds.TD)
 
+    if conf.TM_FEED:
+        collector.subscribe(Feeds.TM)
+
     while(1):
         time.sleep(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
